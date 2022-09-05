42d84c3f4fec9bdefd1fbc9ae199c7a4cf538ed8
kauangouveia <kauangouveia64@gmail.com>
2022-09-05 00:18:21 -0300

    chore: create structure for home

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/Header/Header"
+
+export const Home = () => {
+    return (
+        <>
+            <div className="w-320 h-screen">
+                <Header/>
+            </div>
+        </>
+    )
+}
